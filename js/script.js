785e0c9679123b7352fd3bf1fd3af1ddb1a31b6f
Norbert Kloc <norb906@gmail.com>
2025-04-03 00:30:58 +0200

    chore: remove comments and clean up

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// document.getElementById('test-button').addEventListener('click', function () {
-// 	const links = document.querySelectorAll('.titles a');
-// 	console.log('links:', links);
-// });
-
 const titleClickHandler = function (event) {
 	console.log('Link was clicked!');
 	console.log(event);
@@ -12,31 +7,25 @@ const titleClickHandler = function (event) {
 	event.preventDefault();
 	const clickedElement = this;
 
-	/* [DONE] remove class 'active' from all article links  */
 	const activeLinks = document.querySelectorAll('.titles a.active');
 	for (let activeLink of activeLinks) {
 		activeLink.classList.remove('active');
 	}
 
-	/* [IN PROGRESS] add class 'active' to the clicked link */
 	clickedElement.classList.add('active');
 	console.log('clickedElement:', clickedElement);
 
-	/* [DONE] remove class 'active' from all articles */
 	const activeArticles = document.querySelectorAll('article.active');
 	for (let activeArticle of activeArticles) {
 		activeArticle.classList.remove('active');
 	}
 
-	/* get 'href' attribute from the clicked link */
 	const articleSelector = clickedElement.getAttribute('href');
 	console.log('articleSelector:', articleSelector);
 
-	/* find the correct article using the selector (value of 'href' attribute) */
 	const targetArticle = document.querySelector(articleSelector);
 	console.log('targetArticle:', targetArticle);
 
-	/* add class 'active' to the correct article */
 	targetArticle.classList.add('active');
 };
 
